013555b2878d384dae5feed44f65f808c522c8d2
Brillianta Zayyan Muhamad <brilliantazayyan@gmail.com>
2023-03-05 08:16:48 +0700

    index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
-import styles from "@/styles/Home.module.css";
 import { Navbar } from "@/components/navbar";
 import { Footer } from "@/components/footer";
 
@@ -14,7 +13,14 @@ export default function Home() {
       <div>
         <main className="flex flex-col justify-between items-center p-24 min-h-screen bg-zinc-400">
           <div className="flex w-3/4 container flex-col mr-auto py-28 relative">
-            <h1 className=" text-5xl font-sans text-left">
+            <Image
+              src="/cone.png"
+              alt="Picture of the author"
+              layout="fill" // required
+              objectFit="cover" // change to suit your needs
+              className="ml-96" // just an example
+            />
+            <h1 className="text-5xl font-sans text-left">
               Temukan Lahan Parkiran Terdekat Denganmu
             </h1>
             <div className="py-5">
