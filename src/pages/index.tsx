51f23be96ccb252e036f40f85b67d79fdb64411c
Brillianta Zayyan Muhamad <brilliantazayyan@gmail.com>
2023-03-07 07:46:56 +0700

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,36 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import { Inter } from '@next/font/google';
-import { Navbar } from '@/components/navbar';
-import { Footer } from '@/components/footer';
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import { Navbar } from "@/components/navbar";
+import { Footer } from "@/components/footer";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <div>
-        <main className="flex flex-col justify-between items-center p-24 min-h-screen bg-zinc-400">
-          <div className="flex w-3/4 container flex-col mr-auto py-28 relative">
-            <Image
+        <main className="flex flex-col justify-between items-center p-28 min-h-screen bg-zinc-400">
+          <div className="flex w-1/2 container flex-col mr-auto py-1 relative">
+            {/* <Image
               src="/cone.png"
               alt="Picture of the author"
               width="320"
               height="120"
               objectFit="cover" // change to suit your needs
               className="ml-96" // just an example
-            />
-            <h1 className="text-5xl font-sans text-left">Temukan Lahan Parkiran Terdekat Denganmu</h1>
+            /> */}
+            <h1 className="text-5xl font-sans text-left">
+              Temukan Lahan Parkiran Terdekat Denganmu
+            </h1>
             <div className="py-5">
-              <h2 className="text-xl font-sans text-left">Mempermudah anda dalam menemukan lahan parkir.</h2>
-              <p className="text-s font-sans text-left">Segera daftarkan diri anda jika belum mempunyai akun</p>
+              <h2 className="text-xl font-sans text-left">
+                Mempermudah anda dalam menemukan lahan parkir.
+              </h2>
+              <p className="text-s font-sans text-left">
+                Segera daftarkan diri anda jika belum mempunyai akun
+              </p>
             </div>
             <div className="py-12">
               <button
