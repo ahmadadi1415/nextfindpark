e217f35698c6ab38171c09445d91f925a49e7f4a
DifaHananta <103304704+DifaHananta@users.noreply.github.com>
2023-03-14 23:02:16 +0700

    Update Home
    Done

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,11 +1,11 @@
-import { useSession } from "next-auth/react";
-import Link from "next/link";
+import { useSession } from 'next-auth/react';
+import Link from 'next/link';
 
 export function Navbar() {
   const session = useSession();
 
   return (
-    <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-600">
+    <nav className="bg-gradient-to-r from-white to-blue-700 border-gray-200 px-2 sm:px-4 py-2.5 rounded">
       <div className="container flex flex-wrap items-center justify-between mx-auto">
         <Link href="/" className="flex items-center">
           {/* <img
@@ -13,9 +13,7 @@ export function Navbar() {
             className="h-6 mr-3 sm:h-9"
             alt="FindPark Logo"
           /> */}
-          <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-            FindPark
-          </span>
+          <span className="self-center text-xl font-bold whitespace-nowrap text-black">FindPark</span>
         </Link>
         <button
           data-collapse-toggle="navbar-default"
@@ -26,28 +24,14 @@ export function Navbar() {
           aria-expanded="false"
         >
           <span className="sr-only">Open main menu</span>
-          <svg
-            className="w-6 h-6"
-            aria-hidden="true"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-              clip-rule="evenodd"
-            ></path>
+          <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path>
           </svg>
         </button>
         <div className="hidden w-full md:block md:w-auto" id="navbar-default">
           <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-600 dark:border-gray-700">
             <li>
-              <a
-                href="#"
-                className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-green md:p-0 dark:text-white"
-                aria-current="page"
-              >
+              <a href="#" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-green md:p-0 dark:text-white" aria-current="page">
                 Home
               </a>
             </li>
@@ -69,19 +53,10 @@ export function Navbar() {
             </li>
             {session.data?.user?.name && (
               <li>
-                <div
-                  id="dropdownHover"
-                  className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
-                >
-                  <ul
-                    className="py-2 text-sm text-gray-700 dark:text-gray-200"
-                    aria-labelledby="dropdownHoverButton"
-                  >
+                <div id="dropdownHover" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+                  <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownHoverButton">
                     <li>
-                      <a
-                        href="#"
-                        className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                      >
+                      <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
                         Sign out
                       </a>
                     </li>
@@ -93,19 +68,10 @@ export function Navbar() {
                 >
                   Hai, {session.data?.user?.name}
                 </a>
-                <div
-                  id="dropdownHover"
-                  className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
-                >
-                  <ul
-                    className="py-2 text-sm text-gray-700 dark:text-gray-200"
-                    aria-labelledby="dropdownHoverButton"
-                  >
+                <div id="dropdownHover" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+                  <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownHoverButton">
                     <li>
-                      <a
-                        href="#"
-                        className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                      >
+                      <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
                         Sign out
                       </a>
                     </li>
--- src/pages/users/home.tsx
+++ src/pages/users/home.tsx
@@ -4,7 +4,7 @@ import { Inter } from '@next/font/google';
 import styles from '@/styles/Home.module.css';
 import { Navbar } from '@/components/navbar';
 import { Footer } from '@/components/footer';
-import { Recomcard } from '@/components/recomcard';
+import { Findcard, Recomcard } from '@/components/recomcard';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -13,15 +13,64 @@ export default function Home() {
     <>
       <Navbar />
       <div>
-        <main className="p-24 flex flex-col-3 items-center justify-between min-h-screen bg-white">
-          <div>
-            <Recomcard />
+        <main className="px-10 flex min-h-screen bg-white">
+          <div className="">
+            <div className="container text-black">
+              <h1 className="text-center py-5 font-bold text-3xl">REKOMENDASI</h1>
+              <div className="pb-5">
+                <Recomcard />
+              </div>
+              <div className="pb-5">
+                <Recomcard />
+              </div>
+            </div>
           </div>
-          <div>
-            <Recomcard />
-          </div>
-          <div>
-            <Recomcard />
+          <div className="container py-5">
+            <div className="container pl-10">
+              <div className=" bg-gray-300 rounded-xl">
+                <div className="flex justify-center py-5">
+                  <div>
+                    <input type="text" className="w-96 rounded-xl" />
+                  </div>
+                </div>
+                <div className="flex px-5 text-blue-800">
+                  <div className="w-12">
+                    <img src="/lokasi.png" alt="" />
+                  </div>
+                  <div className="w-96">
+                    <p>Graha Cakrawala Universitas Negeri Malang Jl. Cakrawala, Sumbersari, Lowokwaru, Kota Malang</p>
+                  </div>
+                  <div className="flex items-center text-green-700 pl-20">
+                    <div className="text-3xl">
+                      <button className="border-8 border-green-700 w-56 h-14 rounded-full">TERDEKAT</button>
+                    </div>
+                    <div className="text-3xl pl-10">
+                      <button className="border-8 border-green-700 w-56 h-14 rounded-full">TIDAK RAMAI</button>
+                    </div>
+                  </div>
+                </div>
+                <div className="grid grid-cols-2 pl-5 pt-5">
+                  <div className="pr-5 pb-5">
+                    <Findcard />
+                  </div>
+                  <div className="pr-5 pb-5">
+                    <Findcard />
+                  </div>
+                  <div className="pr-5 pb-5">
+                    <Findcard />
+                  </div>
+                  <div className="pr-5 pb-5">
+                    <Findcard />
+                  </div>
+                  <div className="pr-5 pb-5">
+                    <Findcard />
+                  </div>
+                  <div className="pr-5 pb-5">
+                    <Findcard />
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </main>
       </div>
