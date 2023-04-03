6fe5a47b17efde512b49632809daedd54518463e
Brillianta Zayyan Muhamad <brilliantazayyan@gmail.com>
2023-04-03 11:21:16 +0700

    update

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,14 +1,16 @@
-import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
-import GithubProvider from "next-auth/providers/github"
-import GoogleProvider from "next-auth/providers/google"
-import CredentialsProvider from "next-auth/providers/credentials"
-import EmailProvider, { SendVerificationRequestParams } from "next-auth/providers/email"
-import prisma from "lib/prisma"
-import { compare } from "bcrypt"
-import NextAuth, { Account, Profile, User } from "next-auth"
-import { signIn } from "next-auth/react"
-import cloudinary from "@/utils/cloudinary"
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+import EmailProvider, {
+  SendVerificationRequestParams,
+} from "next-auth/providers/email";
+import prisma from "lib/prisma";
+import { compare } from "bcrypt";
+import NextAuth, { Account, Profile, User } from "next-auth";
+import { signIn } from "next-auth/react";
+import cloudinary from "@/utils/cloudinary";
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -19,11 +21,10 @@ export default NextAuth({
         port: parseInt(process.env.EMAIL_SERVER_PORT as string),
         auth: {
           user: process.env.EMAIL_SERVER_USER,
-          pass: process.env.EMAIL_SERVER_PASSWORD
+          pass: process.env.EMAIL_SERVER_PASSWORD,
         },
       },
       from: process.env.EMAIL_FROM,
-
     }),
     GithubProvider({
       clientId: process.env.GITHUB_ID ?? "",
@@ -40,48 +41,48 @@ export default NextAuth({
       credentials: {
         email: {
           label: "Email",
-          type: "text"
+          type: "text",
         },
         password: {
           label: "Password",
-          type: "password"
-        }
+          type: "password",
+        },
       },
 
       async authorize(credentials) {
-        await prisma.$connect()
+        await prisma.$connect();
 
         // Finding user that correspond with the email
         const user: any = await prisma.user.findUnique({
           where: {
-            email: credentials?.email
-          }
-        })
+            email: credentials?.email,
+          },
+        });
 
         // If email not found
         if (!user) {
-          console.log("No email")
-          throw new Error("Email is unregistered")
+          console.log("No email");
+          throw new Error("Email is unregistered");
         }
 
         // Check hashed password with the database hashed password
         const isPasswordCorrect = await compare(
-          credentials!.password, user.password
-        )
+          credentials!.password,
+          user.password
+        );
 
         // If password incorrect
         if (!isPasswordCorrect) {
-          throw new Error("Password is incorrect")
+          throw new Error("Password is incorrect");
         }
 
-        return user
-      }
-    })
+        return user;
+      },
+    }),
   ],
   pages: {
     signIn: "/login",
     verifyRequest: "/verification",
-
   },
   debug: process.env.NODE_ENV === "development",
   adapter: PrismaAdapter(prisma),
@@ -89,46 +90,46 @@ export default NextAuth({
     strategy: "jwt",
     maxAge: 60 * 60 * 24,
   },
+
   jwt: {
     secret: process.env.NEXTAUTH_SECRET,
   },
   callbacks: {
     async jwt({ token, user, isNewUser }) {
       if (user) {
-        token.role = user.role
-        token.uid = user.id
+        token.role = user.role;
+        token.uid = user.id;
       }
-      return token
+      return token;
     },
     async session({ session, token, user }) {
       if (token && session.user) {
-        session.user.role = token.role
-        session.user.id = token.uid as string
+        session.user.role = token.role;
+        session.user.id = token.uid as string;
 
         const profile = await prisma.profile.findUnique({
           where: {
-            user_id: token.uid as string
+            user_id: token.uid as string,
           },
           select: {
-            photo: true
-          }
-        })
+            photo: true,
+          },
+        });
 
-        let photoUrl = null
+        let photoUrl = null;
         try {
-          await cloudinary.api.resource(profile?.photo as string)
+          await cloudinary.api
+            .resource(profile?.photo as string)
             .then((result) => {
-              photoUrl = (JSON.parse(JSON.stringify(result))).secure_url
-            })
+              photoUrl = JSON.parse(JSON.stringify(result)).secure_url;
+            });
         } catch (error) {
-          console.log(error)
+          console.log(error);
         }
 
-        session.user.image = photoUrl
+        session.user.image = photoUrl;
       }
-      return session
+      return session;
     },
-    
-  }
-
-})
+  },
+});
