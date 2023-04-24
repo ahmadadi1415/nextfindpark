cb36d5529d18c067f630eda8d48006b9338730b1
DifaHananta <103304704+DifaHananta@users.noreply.github.com>
2023-04-24 12:04:52 +0700

    Update Admin
    Proses

--- src/components/navbaradmin.tsx
+++ src/components/navbaradmin.tsx
@@ -1,6 +1,6 @@
 export function Navbaradmin() {
   return (
-    <nav className="fixed top-0 z-50 w-full bg-white border-b border-blue-200 dark:bg-blue-800 dark:border-blue-700">
+    <nav className=" top-0 z-50 bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
       <div className="px-3 py-3 lg:px-5 lg:pl-3">
         <div className="flex items-center justify-between">
           <div className="flex items-center justify-start">
@@ -9,16 +9,10 @@ export function Navbaradmin() {
               data-drawer-toggle="logo-sidebar"
               aria-controls="logo-sidebar"
               type="button"
-              className="inline-flex items-center p-2 text-sm text-blue-500 rounded-lg md:hidden hover:bg-blue-100 focus:outline-none focus:ring-2 focus:ring-blue-200 dark:text-blue-400 dark:hover:bg-blue-700 dark:focus:ring-blue-600"
+              className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
             >
               <span className="sr-only">Open sidebar</span>
-              <svg
-                className="w-6 h-6"
-                aria-hidden="true"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
+              <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                 <path
                   clip-rule="evenodd"
                   fill-rule="evenodd"
@@ -27,9 +21,7 @@ export function Navbaradmin() {
               </svg>
             </button>
             <a href="" className="flex ml-2 md:mr-24">
-              <span className="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">
-                FindPark
-              </span>
+              <span className="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">FindPark</span>
             </a>
           </div>
         </div>
