ff47b1cb46b624e183d75580b5d13cc5a9b637ee
DifaHananta <103304704+DifaHananta@users.noreply.github.com>
2023-04-26 07:57:55 +0700

    Update warna
    belum fix

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,8 +1,8 @@
 export function Footer() {
   return (
-    <footer className="p-4 bg-gradient-to-r from-white to-blue-700 shadow md:flex md:items-center md:justify-between md:p-6">
+    <footer className="p-4 bg-gradient-to-r from-abu to-birugelap shadow md:flex md:items-center md:justify-between md:p-6">
       <span className="text-sm text-black sm:text-center">
-        © 2023 <a className="hover:underline ">FindPark</a>. All Rights Reserved.
+        © 2023 <a className="hover:underline text-unguterang">FindPark</a>. All Rights Reserved.
       </span>
     </footer>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
 
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      birugelap: '#252FE5',
+      biruterang: '#5667E2',
+      abu: '#BCC9ED',
+      unguterang: '#F6E9FF',
+      ungugelap: '#8C16F2',
+    },
   },
-  plugins: [require("@tailwindcss/forms")],
+  plugins: [require('@tailwindcss/forms')],
 };
