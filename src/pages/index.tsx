07a4b88dca3b0957dc697f6bc06cea5d35d4de6b
DifaHananta <103304704+DifaHananta@users.noreply.github.com>
2023-04-29 00:12:21 +0700

    Update
    Sedikit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,26 +15,23 @@ export default function Home() {
     <>
       <Navbar />
       <div>
-        <main className="lg:p-32 flex items-center justify-between flex-col-2 min-h-screen bg-gradient-to-r from-abu to-birugelap">
+        <main className="lg:p-32 flex items-center justify-between flex-col-2 min-h-screen bg-gradient-to-r from-white to-blue-800">
           <div className="lg:grid grid-cols-2 ">
             <div className="">
               <div className="mx-auto flex-col flex w-3/4 py-5 ">
-                <h1 className="antialiased text-6xl font-sans text-left text-unguterang font-bold">Temukan Lahan Parkiran Terdekat Denganmu</h1>
+                <h1 className="antialiased text-6xl font-sans text-left text-black font-bold">Temukan Lahan Parkiran Terdekat Denganmu</h1>
                 <div className="py-5">
-                  <h2 className="text-xl font-sans text-left text-unguterang font-bold">Mempermudah anda dalam menemukan lahan parkir.</h2>
-                  <p className="text-s font-sans text-left text-unguterang">Segera daftarkan diri anda jika belum mempunyai akun</p>
+                  <h2 className="text-xl font-sans text-left text-black font-bold">Mempermudah anda dalam menemukan lahan parkir.</h2>
+                  <p className="text-s font-sans text-left text-black">Segera daftarkan diri anda jika belum mempunyai akun</p>
                 </div>
                 <div className="flex lg:py-6 ">
                   <Link href="/login">
-                    <button
-                      type="button"
-                      className="h-12 w-36 inline-block text-unguterang bg-primary hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-xl text-m mr-2 mb-2 bg-ungugelap focus:outline-none dark:focus:ring-blue-800"
-                    >
+                    <button type="button" className="h-12 w-36 inline-block text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-xl text-m mr-2 mb-2 bg-yellow-500 focus:outline-none dark:focus:ring-blue-800">
                       Login
                     </button>
                   </Link>
                   <Link href="/registration">
-                    <button type="button" className="ml-16 h-12 w-36 inline-block text-unguterang bg-gray-200 hover:bg-gray-300 focus:ring-4 focus:ring-blue-300 font-medium rounded-xl text-m  mr-2 mb-2 bg-ungugelap ">
+                    <button type="button" className="ml-16 h-12 w-36 inline-block text-white bg-yellow-500 hover:bg-gray-300 focus:ring-4 focus:ring-blue-300 font-medium rounded-xl text-m  mr-2 mb-2 bg-ungugelap ">
                       Daftar
                     </button>
                   </Link>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,6 @@ module.exports = {
 
   theme: {
     extend: {},
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      birugelap: '#252FE5',
-      biruterang: '#5667E2',
-      abu: '#BCC9ED',
-      unguterang: '#F6E9FF',
-      ungugelap: '#8C16F2',
-    },
   },
   plugins: [require('@tailwindcss/forms')],
 };
