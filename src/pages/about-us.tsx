fef6006342adf65fa1fcb9e5d2aabdbe3c40b893
Brillianta Zayyan Muhamad <brilliantazayyan@gmail.com>
2023-04-28 08:57:43 +0700

    Update about-us.tsx

--- src/pages/about-us.tsx
+++ src/pages/about-us.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 import Navbar from "@/components/navbar";
 
 
-export default function Aboutus(props : any) {
+export default function Aboutus() {
   return (
     <>
      
