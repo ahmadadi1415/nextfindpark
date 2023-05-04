1cf966fcb5819c710c04d76a4eb6223e2f42d236
DifaHananta <103304704+DifaHananta@users.noreply.github.com>
2023-05-04 19:04:24 +0700

    Update login.tsx
    fix

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,21 +1,21 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "@next/font/google";
-import Navbar from "@/components/navbar";
-import { Footer } from "@/components/footer";
-import { ErrorMessage, Field, Form, Formik, FormikValues } from "formik";
-import { NextPage, InferGetServerSidePropsType } from "next";
-import axios from "axios";
-import { getProviders, signIn } from "next-auth/react";
-import Router, { useRouter } from "next/router";
-import { use, useState } from "react";
-import Link from "next/link";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import { useSession } from "next-auth/react";
-import { getSession } from "next-auth/react";
+import Head from 'next/head';
+import Image from 'next/image';
+import { Inter } from '@next/font/google';
+import Navbar from '@/components/navbar';
+import { Footer } from '@/components/footer';
+import { ErrorMessage, Field, Form, Formik, FormikValues } from 'formik';
+import { NextPage, InferGetServerSidePropsType } from 'next';
+import axios from 'axios';
+import { getProviders, signIn } from 'next-auth/react';
+import Router, { useRouter } from 'next/router';
+import { use, useState } from 'react';
+import Link from 'next/link';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useSession } from 'next-auth/react';
+import { getSession } from 'next-auth/react';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 const Login: NextPage = ({ providers }: any) => {
   const [showPassword, setShowPassword] = useState(false);
 
@@ -24,17 +24,17 @@ const Login: NextPage = ({ providers }: any) => {
     <div className="mt-5">
       {Object.values(providers).map(
         (provider: any) =>
-          provider.name !== "Credentials" &&
-          provider.name !== "Email" && (
+          provider.name !== 'Credentials' &&
+          provider.name !== 'Email' && (
             <button
               key={provider.name}
               onClick={() => {
                 signIn(provider.id, {
-                  callbackUrl: "/redirect",
+                  callbackUrl: '/redirect',
                 });
               }}
             >
-              <p className="text-black">Sign In with {provider.name}</p>
+              <p className="text-black">Sign In with {provider.name}|</p>
             </button>
           )
       )}
@@ -55,25 +55,23 @@ const Login: NextPage = ({ providers }: any) => {
         <main className="lg:p-36 px-2 flex items-center justify-between min-h-screen bg-white ">
           <div>
             <Formik
-              initialValues={{ email: "", password: "" }}
+              initialValues={{ email: '', password: '' }}
               validateOnChange={true}
               validateOnBlur={true}
               validate={(values) => {
                 const errors: any = {};
                 if (!values.email) {
-                  errors.email = "Required";
-                } else if (
-                  !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-                ) {
-                  errors.email = "Invalid Email Address";
+                  errors.email = 'Required';
+                } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+                  errors.email = 'Invalid Email Address';
                 }
                 if (!values.password) {
-                  errors.password = "Required";
+                  errors.password = 'Required';
                 }
                 return errors;
               }}
               onSubmit={(values, actions) => {
-                console.log("onSubmit");
+                console.log('onSubmit');
                 loginUser(values, actions);
               }}
             >
@@ -81,35 +79,18 @@ const Login: NextPage = ({ providers }: any) => {
                 <Form>
                   <div>
                     <div>
-                      <p className="text-5xl text-amber-900  pb-5 font-bold">
-                        Halo!
-                      </p>
-                      <p className="text-xl text-black pb-7">
-                        Kamu harus login dulu nih sebelum pakai app-nya
-                      </p>
+                      <p className="text-5xl text-black  pb-5 font-bold">Halo!</p>
+                      <p className="text-xl text-black pb-7">Kamu harus login dulu nih sebelum pakai app-nya</p>
                     </div>
 
                     <Field name="email">
                       {() => (
                         <div className="pb-2">
-                          <input
-                            type="email"
-                            name="email"
-                            className="rounded-lg w-96 text-black"
-                            value={props.values.email}
-                            onChange={props.handleChange}
-                            onBlur={props.handleBlur}
-                            placeholder="Email"
-                          />
+                          <input type="email" name="email" className="rounded-lg w-96 text-black" value={props.values.email} onChange={props.handleChange} onBlur={props.handleBlur} placeholder="Email" />
                           {props.touched.email && props.errors.email && (
                             <div className="mt-3 inline-flex w-1/2 items-center rounded-lg bg-red-100 py-1 px-2 text-base text-red-700">
                               <span className="mr-2">
-                                <svg
-                                  xmlns="http://www.w3.org/2000/svg"
-                                  viewBox="0 0 24 24"
-                                  fill="currentColor"
-                                  className="h-6 w-6"
-                                >
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-6 w-6">
                                   <path
                                     fill-rule="evenodd"
                                     d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 01.67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 11-.671-1.34l.041-.022zM12 9a.75.75 0 100-1.5.75.75 0 000 1.5z"
@@ -128,7 +109,7 @@ const Login: NextPage = ({ providers }: any) => {
                       {() => (
                         <div className="pb-2">
                           <input
-                            type={showPassword ? "text" : "password"}
+                            type={showPassword ? 'text' : 'password'}
                             name="password"
                             className="rounded-lg w-96 text-black"
                             value={props.values.password}
@@ -139,12 +120,7 @@ const Login: NextPage = ({ providers }: any) => {
                           {props.touched.password && props.errors.password && (
                             <div className="mt-3 inline-flex w-1/2 items-center rounded-lg bg-red-100 py-1 px-2 text-base text-red-700">
                               <span className="mr-2">
-                                <svg
-                                  xmlns="http://www.w3.org/2000/svg"
-                                  viewBox="0 0 24 24"
-                                  fill="currentColor"
-                                  className="h-6 w-6"
-                                >
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-6 w-6">
                                   <path
                                     fill-rule="evenodd"
                                     d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 01.67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 11-.671-1.34l.041-.022zM12 9a.75.75 0 100-1.5.75.75 0 000 1.5z"
@@ -162,14 +138,7 @@ const Login: NextPage = ({ providers }: any) => {
                   <div className="grid grid-cols-2 pb-7">
                     <div className="text-black">
                       <p>
-                        <input
-                          className="form-checkbox rounded-full mr-3"
-                          type="checkbox"
-                          onClick={(e) =>
-                            setShowPassword(e.currentTarget.checked)
-                          }
-                        />{" "}
-                        Tampilkan Kata Sandi
+                        <input className="form-checkbox rounded-full mr-3" type="checkbox" onClick={(e) => setShowPassword(e.currentTarget.checked)} /> Tampilkan Kata Sandi
                       </p>
                     </div>
                     <div className="text-gray-700 underline text-right ">
@@ -178,7 +147,7 @@ const Login: NextPage = ({ providers }: any) => {
                     <p className="text-black text-right font-bold"></p>
                   </div>
                   <div>
-                    <div className="bg-purple-800 rounded-lg font-bold text-center w-44 h-12">
+                    <div className="bg-yellow-500 hover:bg-yellow-600 rounded-full  font-bold text-center w-44 h-12">
                       <button className="py-3" type="submit">
                         Login
                       </button>
@@ -219,11 +188,11 @@ export async function getServerSideProps() {
   };
 }
 
-const redirectToHome = async() => {
+const redirectToHome = async () => {
   const router = Router;
   const { pathname } = router;
-  if (pathname === "/login") {
-    router.push("/redirect");
+  if (pathname === '/login') {
+    router.push('/redirect');
   }
 };
 
@@ -233,44 +202,38 @@ export const loginUser = async (values: FormikValues, actions: any) => {
   // If email is not verified, redirect to verification page
   // Else login using Credentials
 
-  const loginInfo = await axios.post(
-    "/api/auth/check-login",
-    JSON.stringify(values),
-    {
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-    }
-  );
+  const loginInfo = await axios.post('/api/auth/check-login', JSON.stringify(values), {
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  });
 
   console.log(loginInfo);
   if (loginInfo.data.emailVerified === null) {
     // If the email is not verified, sign in using email first
     // then verify email after user click link
 
-    console.log("not verified");
+    console.log('not verified');
 
     // Redirect to verification page
     if (loginInfo.data.hasVerifToken === false) {
-      const res: any = await signIn("email", {
+      const res: any = await signIn('email', {
         email: values.email,
         password: values.password,
         redirect: false,
         callbackUrl: `${window.location.origin}`,
       });
 
-      res.error
-        ? toast.error(res.error)
-        : toast.info("Harap cek email verifikasi anda");
+      res.error ? toast.error(res.error) : toast.info('Harap cek email verifikasi anda');
     } else {
-      toast.info("Harap cek email verifikasi anda");
-      console.log("Please check your email");
+      toast.info('Harap cek email verifikasi anda');
+      console.log('Please check your email');
     }
   } else {
     // If the email is verified, sign in using credentials
-    console.log("verified");
-    const res = await signIn("credentials", {
+    console.log('verified');
+    const res = await signIn('credentials', {
       email: values.email,
       password: values.password,
       redirect: false,
